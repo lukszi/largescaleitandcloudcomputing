347d3c23165f1ebe2d997ba231f310b13d83e502
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-06-29 19:23:39 +0200

    Stop using protected keyword user in user database

--- database/user/user_schema.sql
+++ database/user/user_schema.sql
@@ -1,13 +1,15 @@
 -- -----------------------------------------------------
 -- Schema chat_service
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS "user";
+CREATE SCHEMA IF NOT EXISTS "users";
 
 -- -----------------------------------------------------
--- Table "user"."user"
+-- Table "users"."users"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS "user"."user" (
-    user_id SERIAL NOT NULL,
-    "name" VARCHAR(45) NOT NULL,
-    "password" bytea NOT NULL,
-    PRIMARY KEY (user_id));
+CREATE TABLE IF NOT EXISTS "users"."users" (
+    "user_id" SERIAL NOT NULL,
+    "name" VARCHAR(45) NOT NULL UNIQUE,
+    "password" VARCHAR(60) NOT NULL,
+    PRIMARY KEY ("user_id"));
+
+INSERT INTO "users"."users" (name, password) VALUES ('Lukas', '$2a$12$WTHbLMXJZ9WuNmTJ16Zq3uQAPoL.nVM99y/gDjUu.KvgWF1fqBg0q')
