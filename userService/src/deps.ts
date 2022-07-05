1c2f1fbb08a4b5694b39fcdcccef4fd3b20a0cb9
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-07-05 11:06:12 +0200

    fix import version not being static

--- userService/src/deps.ts
+++ userService/src/deps.ts
@@ -1,4 +1,4 @@
 export { serve } from "https://deno.land/std@0.145.0/http/server.ts";
 export {Pool, PoolClient, QueryClient, Transaction, TransactionError} from "https://deno.land/x/postgres@v0.16.1/mod.ts";
 export {hash, compare } from "https://deno.land/x/bcrypt@v0.4.0/mod.ts";
-export { urlParse } from 'https://deno.land/x/url_parse/mod.ts';
+export { urlParse } from 'https://deno.land/x/url_parse@1.1.0/mod.ts';
