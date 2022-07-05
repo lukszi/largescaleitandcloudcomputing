f4cd16fd69304233979fed9ea2293851112e7430
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-07-05 11:17:54 +0200

    Handled DBFailure

--- userService/src/web/endpoints.ts
+++ userService/src/web/endpoints.ts
@@ -1,6 +1,7 @@
 import {User} from "../data/user.ts";
 import {Database} from "../data/database.ts";
 import {WebToken} from "../util/types.ts";
+import {UserAlreadyExistsException} from "../util/exceptions.ts";
 
 export function buildCreateUserEndpoint(db: Database): (req: Request, tokenDetails?: WebToken) => Promise<Response>{
     return async function createUser(req: Request): Promise<Response> {
@@ -12,9 +13,18 @@ export function buildCreateUserEndpoint(db: Database): (req: Request, tokenDetai
         // get json from request
         const body = await req.json();
 
-        // TODO: Handle db failure
         const modelUser = await User.create(body.name, body.password);
-        const dbUser = await db.createUser(modelUser);
-        return new Response(dbUser.toJson(), {status: 201});
+        try {
+            const dbUser = await db.createUser(modelUser);
+            return new Response(dbUser.toJson(), {status: 201});
+        }
+        catch (e) {
+            if (e instanceof UserAlreadyExistsException) {
+                return new Response("User already exists", {status: 409});
+            }
+            else {
+                throw new Response("Failed to create user", {status: 500});
+            }
+        }
     }
 }
