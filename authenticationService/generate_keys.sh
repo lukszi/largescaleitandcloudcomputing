408fca96ee96f3e4e79fe50f1cf7e3104dc50f80
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-07-06 20:05:11 +0200

    fix using subshell when switching directories

--- authenticationService/generate_keys.sh
+++ authenticationService/generate_keys.sh
@@ -1,3 +1,3 @@
-cd app/keys || (mkdir -p app/keys; cd app/keys || exit 1)
+cd app/keys || { { mkdir -p app/keys; cd app/keys; } || exit 1; }
 ssh-keygen -t rsa -N '' -b 4096 -m PEM -f jwtRS256.key <<< y
 ssh-keygen -e -m PEM -f jwtRS256.key > jwtRS256.key.pub
