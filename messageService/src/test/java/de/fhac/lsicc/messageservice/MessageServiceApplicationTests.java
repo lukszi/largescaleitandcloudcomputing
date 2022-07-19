eaf124e7ad16898a4a49ade9945ffbb2141b8c5c
Lukas Szimtenings <lszimtenings@ukaachen.de>
2022-07-19 20:21:56 +0200

    Create messageService project

---
+++ messageService/src/test/java/de/fhac/lsicc/messageservice/MessageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fhac.lsicc.messageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
